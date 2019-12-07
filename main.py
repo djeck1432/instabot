e64c018db251812ab3098bb7c67bf113c152fd7b
djeck1432 <djeck1432@users.noreply.github.com>
2019-12-07 18:16:57 +0100

    change functions on the comprehensions

--- main.py
+++ main.py
@@ -5,50 +5,19 @@ import re
 import os
 
 
-# В коде, для поиска комментариев, которые выполнили условие, мы используем регулярные выражение :
-# (?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)
-# первоисточник и более расширенное описано здесь: 
+#первоисточник и более расширенное описано регулярных выражений здесь:
 #https://blog.jstassen.com/2016/03/code-regex-for-instagram-username-and-hashtags/
+
 def get_comments(comments):
   comments_from_competition = []
   for comment in comments:
-    participants = re.findall(r'(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)',comment['text'])
+    participants = re.findall('(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)',comment['text'])
     comments_from_competition.append([comment['user_id'],participants])
   return comments_from_competition
 
 def is_users_exist(user_name):
   return bot.get_user_id_from_username(user_name)  is not None
 
-def get_users_id_username(users_name):
-  infromation_about_users = []
-  for user,comment in users_name:
-    infromation_about_users.append([bot.get_username_from_user_id(user),user])
-  return infromation_about_users
-
-
-
-
-def get_liked_and_commented(usernames_and_ids):
-    liked_and_commented = []
-    for user_name,id_user in usernames_and_ids:
-        if str(id_user) in all_users_who_liked:
-            liked_and_commented.append(id_user)
-    return liked_and_commented
-
-def get_username_of_participants(liked_and_commented):
-    username_of_participants = []
-    for user in set(liked_and_commented):
-        username = bot.get_username_from_user_id(user)
-        username_of_participants.append(username)
-    return username_of_participants
-
-def check_followers(username_of_participants):
-    main_instagram_followers = bot.get_user_followers('beautybar.rus')
-    users_all_done = []
-    for username in username_of_participants:
-        if bot.get_username_from_user_id(username) in main_instagram_followers:
-            users_all_done.append(username)
-    return users_all_done
 
 if __name__ == '__main__':
     load_dotenv()
@@ -62,14 +31,14 @@ if __name__ == '__main__':
         description='Проверяет, кто выполнил условие конкурса в инстаграме'
     )
     parser.add_argument('user_id', help='Введите ссылку на пост розыграша')
+    parser.add_argument('instagram_username', help='Введите названия страницы розыграша')
     args = parser.parse_args()
-
+    main_instagram_followers = bot.get_user_followers(args.instagram_username)
     user_id = bot.get_media_id_from_link(args.user_id)
     user_comments = bot.get_media_comments(user_id)
     users = get_comments(user_comments)
-    usernames_and_ids = get_users_id_username(users)
+    usernames_and_ids = [[bot.get_username_from_user_id(user),user] for user,comment in users]
     all_users_who_liked = bot.get_media_likers(user_id)
-    username_of_participants = get_liked_and_commented(usernames_and_ids)
-    check_followers = get_username_of_participants(username_of_participants)
-    print(check_followers(check_followers))
-
+    username_of_participants = [id_user for user_name,id_user in usernames_and_ids if str(id_user)]
+    check_followers = [bot.get_username_from_user_id(user) for user in set(username_of_participants)]
+    print([username for username in check_followers if bot.get_username_from_user_id(username) in main_instagram_followers])
